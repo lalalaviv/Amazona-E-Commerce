769d82b19bf8301abac4568cf6ad5acad9b4fb81
lalalaviv <lalala.viv@hotmail.com>
2022-11-12 21:38:24 +1100

    added columns to models

--- models/Category.js
+++ models/Category.js
@@ -7,7 +7,22 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+
+    // id column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
   },
+
+  // category_name column
+  category_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+},
+
   {
     sequelize,
     timestamps: false,
